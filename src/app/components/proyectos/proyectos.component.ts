e04cb58282f4b2f0673a89d3f6ab4740ddedd519
javierf39 <javi.freire@duocuc.cl>
2022-11-11 13:13:41 -0400

    nuevo proyecto subido

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -11,8 +11,9 @@ export class ProyectosComponent implements OnInit {
     {nombre:"Weather", descripcion:"Aplicación web de clima utilizando la API OpenWeather", img:"../assets/img/app_clima.PNG", tecnologias:["fa-brands fa-html5 icon", "fa-brands fa-css3 icon","fa-brands fa-js icon"], github: "https://github.com/javierf39/weatherJS", web:'https://javierfreire-clima.netlify.app'},
     {nombre:"Animes", descripcion:"Aplicación web utilizando Jikan API.", img:"../assets/img/app_anime.PNG", tecnologias:["fa-brands fa-html5 icon","fa-brands fa-css3 icon","fa-brands fa-angular icon"],github:"https://github.com/javierf39/apiAnimeAngular",web:'https://animes-jf.netlify.app'},
     {nombre:"Películas", descripcion:"Aplicación web, basada en un CRUD, con desarrollo de FronEnd y BackEnd.", img:"../assets/img/pelicula.PNG",tecnologias:["fa-brands fa-html5 icon","fa-brands fa-css3 icon","fa-brands fa-angular icon", "fa-brands fa-node-js icon"], github:"https://github.com/javierf39/peliculasAngular", web:null},
+    {nombre:"Tienda", descripcion:"Tienda online, consume los datos de la API dummyJSON", img:"../assets/img/tienda.PNG", tecnologias:["fa-brands fa-html5 icon","fa-brands fa-sass icon","fa-brands fa-angular icon"], github:"https://github.com/javierf39/tienda_angular", web:"https://tienda-jf.netlify.app"},
     {nombre:"Mundo Patitas", descripcion:"Aplicación web para gestionar horas medicas en clínicas veterinarias", img:"../assets/img/mundoPatitas.PNG", tecnologias:["fa-brands fa-html5 icon","fa-brands fa-css3 icon","fa-brands fa-js icon","fa-brands fa-node-js icon","fa-brands fa-python icon"], github:"https://github.com/javierf39/MundoPatitas",web:null},
-  ]
+  ];
 
   constructor() { }
 
